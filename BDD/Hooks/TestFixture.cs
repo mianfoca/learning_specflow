f32298a4e76d7785547e05010cdf92428408dec9
Miguel Angel Fonseca Castro <mianfoca@hotmail.com>
2017-04-07 18:05:32 -0500

    Lesson05: Bindings(Hooks), Before - After Scenario

---
+++ BDD/Hooks/TestFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDD.Hooks
+{
+    [Binding]
+    public sealed class TestFixture
+    {
+        [BeforeScenario]
+        public void BeforeAllScenario()
+        {
+            Console.WriteLine("Calling before All Scenario");
+        }
+
+        [BeforeScenario("myscenario")]
+        public void BeforeMyScenario()
+        {
+            Console.WriteLine("Calling before My Scenario");
+        }
+
+        [AfterScenario("myscenario")]
+        public void AfterMyScenario()
+        {
+            Console.WriteLine("Calling after My Scenario");
+        }
+
+        [AfterScenario]
+        public void AfterAllScenario()
+        {
+            Console.WriteLine("Calling after All Scenario");
+        }
+    }
+}
---
+++ BDD/Steps/Lesson05.cs
@@ -0,0 +1,54 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace BDD.Steps
+{
+    [Binding]
+    public sealed class Lesson05
+    {
+        public readonly Employee employee;
+
+        public Lesson05(Employee employee)
+        {
+            this.employee = employee;
+        }
+
+        [When(@"I fill all the mandatory details in form option 3")]
+        public void WhenIFillAllTheMandatoryDetailsInFormOption_3(Table table)
+        {
+            //Work with Context Injection
+            var employees = table.CreateDynamicSet();
+
+            foreach (var employee in employees)
+            {
+                this.employees.Add(new Employee()
+                {
+                    Name = (string)employee.Name,
+                    Age = (int)employee.Age,
+                    Email = (string)employee.Email,
+                    Phone = (long)employee.Phone
+            });
+            }
+        }
+
+        [Then(@"show mandatory detais added in option 3")]
+        public void ThenShowMandatoryDetaisAddedInOption_3()
+        {
+            //Work with Context Injection
+            foreach (var employee in this.employees)
+            {
+                Console.WriteLine("The Details of Employee: " + employee.Name);
+                Console.WriteLine("*********************");
+                Console.WriteLine(employee.Age);
+                Console.WriteLine(employee.Email);
+                Console.WriteLine(employee.Phone);
+            }
+        }
+
+    }
+}
