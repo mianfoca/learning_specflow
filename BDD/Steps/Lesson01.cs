02089d59e4e5ad8c8adde8e0da9fe2a35c73d574
Miguel Angel Fonseca Castro <mianfoca@hotmail.com>
2017-04-07 16:12:53 -0500

    Lesson01

---
+++ BDD/Steps/Lesson01.cs
@@ -0,0 +1,37 @@
+﻿using Entities.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDD.Steps
+{
+    [Binding]
+    public sealed class Lesson01
+    {
+        private Calculator calculator = new Calculator();
+
+        private int result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.AddNumber(number);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Sum();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int resultExpected)
+        {
+            Assert.AreEqual(resultExpected, result);
+        }
+
+    }
+}
---
+++ Entities/Models/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Calculator
+    {
+        public List<int> numbers { set; private get; }
+
+        public void AddNumber(int number)
+        {
+            if (numbers == null) numbers = new List<int>();
+            numbers.Add(number);
+        }
+
+        public int Sum()
+        {
+            return numbers.Sum();
+        }
+    }
+}
