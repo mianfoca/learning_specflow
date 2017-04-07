3ea6cc905e12feb7809225e5e80454d9ee9314e2
Miguel Angel Fonseca Castro <mianfoca@hotmail.com>
2017-04-07 17:14:11 -0500

    Lesson 04: Working with multiple data using Table and Specflow.Assist.Dynamic, convert implicit

---
+++ BDD/Features/Lesson04.feature
@@ -0,0 +1,15 @@
+﻿Feature: Lesson04
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Create a new employee with mandatary details option 2
+	#Given I have opened my application
+	#Then I should see employee details page
+	When I fill all the mandatory details in form option 2
+	| Name    | Age | Phone      | Email                         |
+	| karthik | 28  | 9098022312 | karthik@executeautomation.com |
+	| John    | 30  | 2134234    | john@executeautomation.com    |
+	| Sam     | 28  | 2342348902 | sam@executeautomation.com     |
+	#And I click the save button
+	#Then I should see all the details saved in my application and DB
---
+++ BDD/Steps/Lesson04.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace BDD.Steps
+{
+    [Binding]
+    public sealed class Lesson04
+    {
+        [When(@"I fill all the mandatory details in form option 2")]
+        public void WhenIFillAllTheMandatoryDetailsInFormOption_2(Table table)
+        {
+            //Work with Dynamic Assist
+            var employees = table.CreateDynamicSet();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("The Details of Employee: " + employee.Name);
+                Console.WriteLine("*********************");
+                Console.WriteLine(employee.Age);
+                Console.WriteLine(employee.Email);
+                Console.WriteLine(employee.Phone);
+            }
+        }
+    }
+}
