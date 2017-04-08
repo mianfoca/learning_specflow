bba28ca3eb51b61695f336f74b9fad70da12553f
Miguel Angel Fonseca Castro <mianfoca@hotmail.com>
2017-04-08 07:31:17 -0500

    Lesson 06: Scope Bindings, Same Step different implementation

---
+++ BDD/Features/Lesson06.feature
@@ -0,0 +1,14 @@
+﻿Feature: Lesson06_1
+	Simple feature to demonstrate customer Login feature
+
+@customer 
+Scenario: Login for customer portal
+	Given I have opened the application
+	And I login to application
+	Then I see customer portal
+
+@user 
+Scenario: Login for user portal
+	Given I have opened the application
+	And I login to application
+	Then I see user portal
---
+++ BDD/Steps/Lesson06.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDD.Steps
+{
+    [Binding]
+    public sealed class Lesson06
+    {
+        [Given(@"I have opened the application")]
+        public void GivenIHaveOpenedTheApplication()
+        {
+            Console.WriteLine("Opened Application");
+        }
+
+        #region Same Step different implementation
+        [Given(@"I login to application")]
+        [Scope(Tag = "customer")]
+        public void GivenILoginToApplicationAsCustomer()
+        {
+            Console.WriteLine("I want Login as Customer");
+        }
+
+        [Given(@"I login to application")]
+        [Scope(Tag ="user")]
+        public void GivenILoginToApplicationAsUser()
+        {
+            Console.WriteLine("I want Login as User");
+        }
+        #endregion
+
+        [Then(@"I see customer portal")]
+        public void ThenISeeCustomerPortal()
+        {
+            Console.WriteLine("I see customer portal");
+        }
+
+        [Then(@"I see user portal")]
+        public void ThenISeeUserPortal()
+        {
+            Console.WriteLine("I see user portal");
+        }
+
+    }
+}
