02a08d9b84b27a3c1839627853c25ecdcba5b840
Miguel Angel Fonseca Castro <mianfoca@hotmail.com>
2017-04-08 07:42:39 -0500

    Lesson 07: Custom Step Argument Transformation

---
+++ BDD/Features/Lesson07.feature
@@ -0,0 +1,11 @@
+﻿Feature: Lesson07
+	Simple feature to demonstrate user Login feature
+
+@admin 
+Scenario: Login for user portal as admin
+	Given I have opened the application
+	And I login to application
+	Then I see last date logged in data is 5 days from current date
+	#And I see the menu likes
+	#| Menu_1 | Menu_2   | Menu_3 | Menu_4   |
+	#| Login  | Settings | Logout | Advanced |
---
+++ BDD/Steps/Lesson07.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDD.Steps
+{
+    [Binding]
+    public sealed class Lesson07
+    {
+        [Given(@"I login to application")]
+        [Scope(Tag = "admin")]
+        public void GivenILoginToApplicationAsCustomer()
+        {
+            Console.WriteLine("I want Login as Admin");
+        }
+
+        //Before Custom Step Argument Transformation
+        //[Then(@"I see last date logged in data is (.*) days from current date")]
+        //public void ThenISeeLastDateLoggedInDataIsDaysFromCurrentDate(int p0)
+        //{
+        //    var correctdate = DateTime.Today.AddDays(p0);
+        //    Console.WriteLine(correctdate);
+        //}
+
+        //After Custom Step Argument Transformation
+        [Then(@"I see last date logged in data is (.* days from current date)")]
+        public void ThenISeeLastDateLoggedInDataIsDaysFromCurrentDate(DateTime correctdate)
+        {
+            Console.WriteLine(correctdate);
+        }
+
+    }
+}
