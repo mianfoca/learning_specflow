e54cbdf44b2faedb0007f1bcdff7fe2378189f01
Miguel Angel Fonseca Castro <mianfoca@hotmail.com>
2017-04-10 11:21:52 -0500

    Lesson 12: pystrings, specifying a larger piece of text

---
+++ BDD/Features/Lesson12.feature
@@ -0,0 +1,13 @@
+﻿Feature: Lesson12
+	Testing pystrings
+
+@mytag
+Scenario: test pystrings
+	Given a blog post named Random with:
+    """
+    Some Title, Eh?
+    ===============
+    Here is the first paragraph of my blog post.
+    Lorem ipsum dolor sit amet, consectetur adipiscing
+    elit.
+    """
---
+++ BDD/Steps/Lesson12.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDD.Steps
+{
+    [Binding]
+    public sealed class Lesson12
+    {
+        [Given(@"a blog post named Random with:")]
+        public void GivenABlogPostNamedRandomWith(string multilineText)
+        {
+            Console.WriteLine("Specifying a larger piece of text");
+            Console.WriteLine(multilineText);
+        }
+    }
+}
